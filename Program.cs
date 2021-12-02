4d5dc1a1abe185b3ad958d67815d63dce1d33852
AsierBreyas <72077655+AsierBreyas@users.noreply.github.com>
2021-12-02 10:38:15 +0100

    Borrando cosas

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ using static System.Console;
 public class InstitutoContext : DbContext
 {
 
-    public DbSet<Alumno> Alumnos {get; set;}
-    public DbSet<Modulo> Modulos {get; set;}
-    public DbSet<Matricula> Matriculas {get; set;}
+    public DbSet<Alumno> Alumnos { get; set; }
+    public DbSet<Modulo> Modulos { get; set; }
+    public DbSet<Matricula> Matriculas { get; set; }
     public string connString { get; private set; }
 
     public InstitutoContext()
@@ -24,12 +24,13 @@ public class InstitutoContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer(connString);
-    protected override void OnModelCreating(ModelBuilder modelBuilder){
-            modelBuilder.Entity<Matricula>().HasIndex(m => new
-            {
-                m.AlumnoId,
-                m.ModuloId
-            }).IsUnique();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Matricula>().HasIndex(m => new
+        {
+            m.AlumnoId,
+            m.ModuloId
+        }).IsUnique();
 
     }
 
@@ -38,11 +39,12 @@ public class Alumno
 {
     [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
     public int AlumnoId { get; set; }
-    public string Nombre {get; set;}
-    public int Edad {get; set;}
-    public decimal Efectivo {get; set;}
-    public string Pelo {get; set;}
-    public List<Matricula> Matriculas {get;} = new List<Matricula>();
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public decimal Efectivo { get; set; }
+    public string Pelo { get; set; }
+    public List<Matricula> Matriculas { get; } = new List<Matricula>();
+    public override string ToString() => $"{AlumnoId}: {Nombre}: {Edad}: {Efectivo}: {Pelo} ";
     /*
 
     */
@@ -51,10 +53,11 @@ public class Modulo
 {
     [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
     public int ModuloId { get; set; }
-    public string Titulo {get; set;}
-    public int Creditos {get; set;}
-    public int Curso {get; set;}
-    public List<Matricula> Matriculas {get;} = new List<Matricula>();
+    public string Titulo { get; set; }
+    public int Creditos { get; set; }
+    public int Curso { get; set; }
+    public List<Matricula> Matriculas { get; } = new List<Matricula>();
+    public override string ToString() => $"{ModuloId}: {Titulo}: {Creditos}: {Curso} ";
     /*
 
     */
@@ -62,11 +65,14 @@ public class Modulo
 }
 public class Matricula
 {
-    public int MatriculaId {get; set;}
-    public int AlumnoId {get; set;}
-    public int ModuloId {get; set;}
-    public Alumno alumno {get; set;}
-    public Matricula matricula {get; set;}
+    public Alumno Alumno { get; set; }
+    public Modulo Modulo { get; set; }
+    public int MatriculaId { get; set; }
+
+    public int AlumnoId { get; set; }
+
+    public int ModuloId { get; set; }
+    public override string ToString() => $"{MatriculaId}: {AlumnoId}: {ModuloId} ";
 }
 
 class Program
@@ -82,35 +88,36 @@ class Program
 
             // Añadir Alumnos
             // Id de 1 a 7
-            db.Add(new Alumno{AlumnoId = 1, Nombre = "Eustaquio", Edad = 75, Efectivo = 60.54M, Pelo = "Castaño"});
-            db.Add(new Alumno{AlumnoId = 2, Nombre = "Habichuela", Edad = 45, Efectivo = 60.54M, Pelo = "Moreno"});
-            db.Add(new Alumno{AlumnoId = 3, Nombre = "El gran mago pajas", Edad = 12, Efectivo = 60.54M, Pelo = "Moreno"});
-            db.Add(new Alumno{AlumnoId = 4, Nombre = "Willy", Edad = 2, Efectivo = 60.54M, Pelo = "Castaño"});
-            db.Add(new Alumno{AlumnoId = 5, Nombre = "Asdrubal", Edad = 17, Efectivo = 60.54M, Pelo = "Rubio"});
-            db.Add(new Alumno{AlumnoId = 6, Nombre = "Astolfo", Edad = 20, Efectivo = 60.54M, Pelo = "Castaño"});
-            db.Add(new Alumno{AlumnoId = 7, Nombre = "Mari Carmen", Edad = 34, Efectivo = 60.54M, Pelo = "Rubio"});
+            db.Add(new Alumno { AlumnoId = 1, Nombre = "Eustaquio", Edad = 75, Efectivo = 60.54M, Pelo = "Castaño" });
+            db.Add(new Alumno { AlumnoId = 2, Nombre = "Habichuela", Edad = 45, Efectivo = 60.54M, Pelo = "Moreno" });
+            db.Add(new Alumno { AlumnoId = 3, Nombre = "El gran mago pajas", Edad = 12, Efectivo = 60.54M, Pelo = "Moreno" });
+            db.Add(new Alumno { AlumnoId = 4, Nombre = "Willy", Edad = 2, Efectivo = 60.54M, Pelo = "Castaño" });
+            db.Add(new Alumno { AlumnoId = 5, Nombre = "Asdrubal", Edad = 17, Efectivo = 60.54M, Pelo = "Rubio" });
+            db.Add(new Alumno { AlumnoId = 6, Nombre = "Astolfo", Edad = 20, Efectivo = 60.54M, Pelo = "Castaño" });
+            db.Add(new Alumno { AlumnoId = 7, Nombre = "Mari Carmen", Edad = 34, Efectivo = 60.54M, Pelo = "Rubio" });
 
             // Añadir Módulos
             // Id de 1 a 10
-            db.Add(new Modulo{ModuloId = 1, Titulo = "Titulo1", Creditos = 20, Curso = 1});
-            db.Add(new Modulo{ModuloId = 2, Titulo = "Titulo2", Creditos = 20, Curso = 2});
-            db.Add(new Modulo{ModuloId = 3, Titulo = "Titulo3", Creditos = 20, Curso = 5});
-            db.Add(new Modulo{ModuloId = 4, Titulo = "Titulo4", Creditos = 20, Curso = 6});
-            db.Add(new Modulo{ModuloId = 5, Titulo = "Titulo5", Creditos = 20, Curso = 3});
-            db.Add(new Modulo{ModuloId = 6, Titulo = "Titulo6", Creditos = 20, Curso = 1});
-            db.Add(new Modulo{ModuloId = 7, Titulo = "Titulo7", Creditos = 20, Curso = 6});
-            db.Add(new Modulo{ModuloId = 8, Titulo = "Titulo8", Creditos = 20, Curso = 5});
-            db.Add(new Modulo{ModuloId = 9, Titulo = "Titulo9", Creditos = 20, Curso = 3});
-            db.Add(new Modulo{ModuloId = 10, Titulo = "Titulo10", Creditos = 20, Curso = 2});
+            db.Add(new Modulo { ModuloId = 1, Titulo = "Titulo1", Creditos = 20, Curso = 1 });
+            db.Add(new Modulo { ModuloId = 2, Titulo = "Titulo2", Creditos = 20, Curso = 2 });
+            db.Add(new Modulo { ModuloId = 3, Titulo = "Titulo3", Creditos = 20, Curso = 5 });
+            db.Add(new Modulo { ModuloId = 4, Titulo = "Titulo4", Creditos = 20, Curso = 6 });
+            db.Add(new Modulo { ModuloId = 5, Titulo = "Titulo5", Creditos = 20, Curso = 3 });
+            db.Add(new Modulo { ModuloId = 6, Titulo = "Titulo6", Creditos = 20, Curso = 1 });
+            db.Add(new Modulo { ModuloId = 7, Titulo = "Titulo7", Creditos = 20, Curso = 6 });
+            db.Add(new Modulo { ModuloId = 8, Titulo = "Titulo8", Creditos = 20, Curso = 5 });
+            db.Add(new Modulo { ModuloId = 9, Titulo = "Titulo9", Creditos = 20, Curso = 3 });
+            db.Add(new Modulo { ModuloId = 10, Titulo = "Titulo10", Creditos = 20, Curso = 2 });
             db.SaveChanges();
             // Matricular Alumnos en Módulos
-            var alumnos = db.Alumnos;
-            var modulos = db.Modulos;
-            foreach(Alumno alumno in alumnos){
-                foreach(Modulo modulo in modulos){
-                    db.Add(new Matricula{AlumnoId = alumno.AlumnoId, ModuloId = modulo.ModuloId});
+            foreach (Alumno alumno in db.Alumnos.ToList())
+            {
+                foreach (Modulo modulo in db.Modulos)
+                {
+                    db.Add(new Matricula { Alumno = alumno, Modulo = modulo });
                 }
             }
+            db.SaveChanges();
 
         }
     }
@@ -118,14 +125,49 @@ class Program
     static void BorrarMatriculaciones()
     {
         // Borrar las matriculas d
-        // AlumnoId multiplo de 3 y ModuloId Multiplo de 2;
-        // AlumnoId multiplo de 2 y ModuloId Multiplo de 5;
+
+        using (var db = new InstitutoContext())
+        {
+            // AlumnoId multiplo de 3 y ModuloId Multiplo de 2;
+            var alumnosM3 = db.Alumnos.Where(x => x.AlumnoId % 3 == 0);
+            var modulosM2 = db.Modulos.Where(x => x.ModuloId % 2 == 0);
+            foreach (Alumno alumno in alumnosM3)
+            {
+                foreach (Modulo modulo in modulosM2)
+                {
+                    var selecMatriculas = db.Matriculas.Where(x => x.AlumnoId == alumno.AlumnoId && x.ModuloId == modulo.ModuloId);
+                    db.Matriculas.RemoveRange(selecMatriculas);
+                }
+            }
+            db.SaveChanges();
+            // AlumnoId multiplo de 2 y ModuloId Multiplo de 5;
+            var alumnosM2 = db.Alumnos.Where(x => x.AlumnoId % 2 == 0);
+            var modulosM5 = db.Modulos.Where(x => x.ModuloId % 5 == 0);
+            foreach (Alumno alumno in alumnosM2)
+            {
+                foreach (Modulo modulo in modulosM5)
+                {
+                    var selecMatriculas = db.Matriculas.SingleOrDefault(x => x.AlumnoId == alumno.AlumnoId && x.ModuloId == modulo.ModuloId);
+                    if (selecMatriculas != null){
+                        db.Matriculas.Remove(selecMatriculas);
+                    }
+                }
+            }
+            db.SaveChanges();
+        }
+
     }
     static void RealizarQuery()
     {
         using (var db = new InstitutoContext())
         {
             // Las queries que se piden en el examen
+            //Filtering
+            var filter = db.Alumnos.Where(o => o.AlumnoId == 1);
+            Console.WriteLine(filter);
+            var filter2 = db.Modulos.Where(o => o.ModuloId == 1);
+            Console.WriteLine(filter2);
+            var filter3 = db.Matriculas.Where(o => o.MatriculaId == 1);
 
         }
     }
